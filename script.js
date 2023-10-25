b427dc6e8248beb87b23ef5907e9669bc96c4aa0
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-25 21:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function toCase(text) {
   // write your code here
+	 if (text.length === 0) {
+        return '-';
+    }
+    
+    const lowercase = text.toLowerCase();
+    const uppercase = text.toUpperCase();
+    
+    return `${lowercase}-${uppercase}`;
 }
 
 // DO not change the code below
